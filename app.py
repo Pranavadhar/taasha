86151591c89344d50b75d0ffaa3e54ffd37c9e2f
Pranavadhar A <113248898+Pranavadhar@users.noreply.github.com>
2025-04-23 08:05:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,8 +77,8 @@ def predict(input_data):
 # Fault detection logic
 def detect_faults(predictions):
     faults = []
-    if predictions[0] > 34:
-        faults.append("Battery Temperature > 34°C - SYSTEM COOLING ACTIVELY")
+    if predictions[0] > 35:
+        faults.append("Battery Temperature > 35°C - SYSTEM COOLING ACTIVELY")
     if predictions[0] > 45:
         faults.append("Battery Temperature > 45°C - SYSTEM OVER HEATING : COOLING SYSTEM CHECK UP RECOMMENDED")
     if predictions[1] < 30:
